bda2c9299ba83226d516278ae4fea333c440538d
hassdbouk <hdbouk@uwo.ca>
2024-11-23 15:03:44 -0500

    c

--- JobAnalysis.js
+++ JobAnalysis.js
@@ -1,20 +1,5 @@
-/*async function readJsonFile(file) {
-    return new Promise((resolve, reject) => {
-      const fileReader = new FileReader()
-      fileReader.onload = event => resolve(JSON.parse(event.target.result))
-      fileReader.onerror = error => reject(error)
-      fileReader.readAsText(file)
-    })
-  }
-  
-const object = await readJsonFile(file)*/
+const jobData = require('./upwork_jobs.json');
+console.log(jobData);
 
-/*const fileSelector = document.querySelector('#file_selector');
 
-fileSelector.addEventListener('change', (event) => {
-    const fileList = event.target.files;
-    console.log(fileList);
-  });*/
 
-const data = JSON.parse("upwork_jobs.json")
-console.log(data[0]);
